d618a2c6f8bb049b2f0df675268fa59519fdcd5b
phucbm <hiphucbui@gmail.com>
2025-07-01 12:42:05 +0700

    remove redundant banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup';
+import {defineConfig} from 'tsup';
 
 export default defineConfig({
     entry: ['src/cli.ts'],
@@ -11,7 +11,5 @@ export default defineConfig({
     sourcemap: false,
     dts: false,
     clean: true,
-    banner: {
-        js: '#!/usr/bin/env node'
-    }
+    banner: {}
 });
